2b8fedf8b5f4104f5755008f5ec8a0476355ba82
Ahmet Mert Sakar <ahmetmert5070@gmail.com>
2021-10-16 18:43:08 +0300

    Merhaba

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MerhabaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
